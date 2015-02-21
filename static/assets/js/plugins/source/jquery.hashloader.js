2a328f4bdeb8e42bf705bd5f795f04895997335e
Luchesar Cekov <luchesar.cekov@gmail.com>
2015-02-21 23:23:42 +0000

    Remove the worchester park pics and add bx slider

--- static/assets/js/plugins/source/jquery.hashloader.js
+++ static/assets/js/plugins/source/jquery.hashloader.js
@@ -155,20 +155,8 @@
 
 function swipeInit() {
     console.log("swipe init");
-    var appendNumber = 4;
-    var prependNumber = 1;
-    var galleryTop = new Swiper('.gallery-top', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 10,
+    $('.bxslider').bxSlider({
+      infiniteLoop: false,
+      hideControlOnEnd: true
     });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        spaceBetween: 10,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        touchRatio: 0.2,
-        slideToClickedSlide: true
-    });
-    galleryTop.params.control = galleryThumbs;
-    galleryThumbs.params.control = galleryTop;
 }
